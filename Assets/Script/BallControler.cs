39ce67a6b2aba927560e10892593626c7908d7d9
Benjamin-Duez <benjamin.duez@hotmail.com>
2024-09-04 15:41:38 +0200

    🚀 camera rotation

--- Assets/Script/BallControler.cs
+++ Assets/Script/BallControler.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
-using UnityEngine.InputSystem;
+using FishNet.Managing.Timing;
+using static UnityEngine.GraphicsBuffer;
 
 public class BallControler : NetworkBehaviour
 {
     [Range(0.0f, 100.0f)]
     [SerializeField] private float force;
+    [SerializeField] private float AbsMagn;
     private Rigidbody rb;
-    public Vector3 sp;
-    public Quaternion sr;
-    
+    private Vector3 sp;
+    private Quaternion sr;
+    [SerializeField] private Camera cam;
+
+    [SerializeField] private Vector3 offset;
+
+    private bool moving;
+    private bool magnHasChanged;
+
+    float zoomLevel;
+    private Vector2 rotationValues;
+    private float RotationSensitivity = 1f;
+
+    private Touch touch;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,27 +37,54 @@ public class BallControler : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        AbsMagn = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z);
+
+        if(!magnHasChanged && AbsMagn > 0.1) magnHasChanged = true;
+        if(magnHasChanged && AbsMagn == 0) magnHasChanged = false;
+
+        if (moving && magnHasChanged && AbsMagn < 1) Stopped();
         
+
+        if (Input.GetMouseButton(1))
+        {
+            var mouseMovement = new Vector2(-Input.GetAxis("Mouse Y") * 3f, Input.GetAxis("Mouse X") * 3f);
+            
+        }
+
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                var mouseMovement = new Vector2(-touch.deltaPosition.y * 3f, touch.deltaPosition.x * 3f);
+                rotationValues += mouseMovement * RotationSensitivity * Time.unscaledDeltaTime;
+                rotationValues = new Vector2(Mathf.Clamp(rotationValues.x, -80f, 80f), rotationValues.y);
+            }
+        }
+
+        var zoomInput = -Input.GetAxis("Mouse ScrollWheel") * 10f;
+        zoomLevel = Mathf.Clamp(zoomLevel + zoomInput, 1f, 10f);
+
+        var curRotation = Quaternion.Euler(rotationValues);
+        var lookPosition = transform.position - (curRotation * Vector3.forward * zoomLevel);
+        cam.transform.SetPositionAndRotation(lookPosition, curRotation);
     }
 
-    //Test
-    public void Avance(InputAction.CallbackContext context)
+    private void OnEnable()
     {
-        //if (!base.Owner)
-          //  return;
-        transform.position += new Vector3(1, 0, 0);
-        AvanceServer();
-    }
-
-    [ServerRpc]
-    private void AvanceServer()
-    {
-        transform.position += new Vector3(1, 0, 0);
+        cam = Camera.main;
+        rotationValues = transform.position + offset;
+        zoomLevel = 10;
     }
 
     public void Push()
     {
-        rb.AddForce(Vector3.left * force, ForceMode.Impulse);
+        var vec = cam.transform.forward;
+        vec = new Vector3(vec.x, 0, vec.z);
+        rb.AddForce(vec * force, ForceMode.Impulse);
+        moving = true;
     }
 
     public void TpStart()
@@ -52,5 +93,23 @@ public class BallControler : NetworkBehaviour
         rb.angularVelocity = Vector3.zero;
         transform.rotation = sr;
         transform.position = sp;
+    }
+
+    private void Stopped()
+    {
+        moving = false;
+        Debug.Log("Ball stopped");
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    public void rotateLeft()
+    {
+        cam.transform.RotateAround(transform.position, Vector3.up, cam.transform.eulerAngles.y - transform.eulerAngles.y);
+    }
+
+    public void rotateLeft(float angle)
+    {
+        cam.transform.RotateAround(transform.position, Vector3.up, angle);
     }
 }
