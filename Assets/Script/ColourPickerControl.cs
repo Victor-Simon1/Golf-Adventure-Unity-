0ba0cfb906cd37aedbaef916f2b489e7a5f084eb
Benjamin-Duez <benjamin.duez@hotmail.com>
2024-09-12 10:05:15 +0200

    :construction:  Colour Picker

---
+++ Assets/Script/ColourPickerControl.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using TMPro.EditorUtilities;
+
+public class ColourPickerControl : MonoBehaviour
+{
+
+    public float currentHue, currentSat, currentVal;
+
+    [SerializeField]
+    private RawImage hueImage, satValImage, outputImage;
+
+    [SerializeField]
+    private Slider hueSlider;
+
+    [SerializeField] 
+    private TMP_InputFieldEditor hexInputField;
+
+    private Texture2D hueTexture, svTexture, outputTexture;
+
+    [SerializeField]
+    MeshRenderer changeThisColour;
+
+    private void Start()
+    {
+        CreateHueImage();
+
+        CreateSVImage();
+
+        CreateOutputImage();
+
+        UpdateOutputImage();
+    }
+
+    private void CreateHueImage()
+    {
+        hueTexture = new Texture2D(1, 16);
+        hueTexture.wrapMode = TextureWrapMode.Clamp;
+        hueTexture.name = "HueTexture";
+
+        for (int i = 0; i < hueTexture.height; i++)
+        {
+            hueTexture.SetPixel(0, i, Color.HSVToRGB((float)i / hueTexture.height,1f, 0.6f));
+        }
+
+        hueTexture.Apply();
+        currentHue = 0;
+
+        hueImage.texture = hueTexture;
+    }
+    
+    private void CreateSVImage()
+    {
+        svTexture = new Texture2D(16, 16);
+        svTexture.wrapMode = TextureWrapMode.Clamp;
+        svTexture.name = "SatValueTexture";
+
+        for (int x = 0; x < svTexture.height; x++)
+        {
+            for (int y = 0; y < svTexture.width; y++)
+            {
+                svTexture.SetPixel(x, y, Color.HSVToRGB(currentHue, (float)x / svTexture.width, (float)y / svTexture.height));
+            }
+        }
+
+        svTexture.Apply();
+
+        currentSat = 0;
+        currentVal = 0;
+
+        satValImage.texture = svTexture;
+    }
+    private void CreateOutputImage()
+    {
+        outputTexture = new Texture2D(1, 16);
+        outputTexture.wrapMode = TextureWrapMode.Clamp;
+        outputTexture.name = "OutputTexture";
+
+        Color currentColour = Color.HSVToRGB(currentHue, currentSat, currentVal);
+
+        for (int i = 0; i < outputTexture.height; i++)
+        {
+            outputTexture.SetPixel(0, i, currentColour);
+        }
+
+        outputTexture.Apply();
+
+        outputImage.texture = outputTexture;
+    }
+
+    private void UpdateOutputImage()
+    {
+        Color currentColour = Color.HSVToRGB(currentHue, currentSat, currentVal);
+
+        for (int i = 0; i < outputTexture.height; i++)
+        {
+            outputTexture.SetPixel(0, i, currentColour);
+        }
+
+        outputTexture.Apply();
+
+        changeThisColour.material.SetColor("_BaseColor", currentColour);
+    }
+
+    public void SetSV(float S,  float V)
+    {
+        currentSat = S;
+        currentVal = V;
+
+        UpdateOutputImage();
+    }
+
+    public void UpdateSVImage()
+    {
+        currentHue = hueSlider.value;
+        for(int y = 0; y < svTexture.height; y++)
+        {
+            for(int x = 0; x < svTexture.width; x++)
+            {
+                svTexture.SetPixel(x, y, Color.HSVToRGB(currentHue, (float)x / svTexture.width, (float)y / svTexture.height));
+            }
+        }
+
+        svTexture.Apply();
+
+        UpdateOutputImage();
+
+    }
+
+}
